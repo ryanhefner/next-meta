f969a7b85f36a626d1687463e050c17bb9f4b62c
Ryan Hefner <hi@ryanhefner.com>
2023-11-28 14:35:47 -0500

    Update types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,7 +1,24 @@
-import { ForwardRefExoticComponent, PropsWithChildren } from 'react'
+interface SiteMetaProps {
+  audioUrl?: string
+  audioType?: string
+  debug?: boolean
+  description?: string
+  determiner?: string
+  imageUrl?: string
+  imageAlt?: string
+  imageWidth?: number
+  imageHeight?: number
+  locale?: string
+  siteName?: string
+  title?: string
+  twitterSite?: string
+  twitterCreator?: string
+  twitterCardType?: string
+  url?: string
+  videoUrl?: string
+  videoType?: string
+}
 
-type TestComponentProps = PropsWithChildren
+declare const SiteMeta: SiteMetaProps
 
-declare const TestComponent: ForwardRefExoticComponent<TestComponentProps>
-
-export default TestComponent
+export default SiteMeta
