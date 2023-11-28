68b41f633d94df12b8c91d71ed674abf5982d635
Ryan Hefner <hi@ryanhefner.com>
2023-11-28 17:00:41 -0500

    Update Vitest coverage reporter config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   test: {
+    coverage: {
+      reporter: ['clover', 'html', 'json', 'lcov'],
+    },
     include: ['./src/**/*.{test,spec}.?(c|m)[jt]s?(x)'],
     environment: 'jsdom'
   },
