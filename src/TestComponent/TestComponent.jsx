250edb80b07398b18cf4876565d3a3196fab132e
Ryan Hefner <hi@ryanhefner.com>
2023-11-28 13:22:26 -0500

    Delete TestComponent

--- src/TestComponent/TestComponent.jsx
+++
@@ -1,9 +0,0 @@
-import React, { forwardRef } from 'react'
-
-const TestComponent = forwardRef((props, ref) => (
-  <div ref={ref}>Test Component</div>
-))
-
-TestComponent.displayName = 'TestComponent'
-
-export default TestComponent
