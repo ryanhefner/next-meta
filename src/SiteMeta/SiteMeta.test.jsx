c56869da93f3b6fbf9af7aa3d1d3f038a7b9adef
Ryan Hefner <hi@ryanhefner.com>
2024-05-13 22:04:35 -0400

    Update test to support new props/tags

--- src/SiteMeta/SiteMeta.test.jsx
+++ src/SiteMeta/SiteMeta.test.jsx
@@ -83,6 +83,13 @@ describe('SiteMeta', () => {
     })
   })
 
+  describe('canonical', () => {
+    test('renders - canonical', () => {
+      const { container } = render(<SiteMeta canonical="https://test.com" />)
+      expect(container.querySelector('[rel="canonical"]')).toBeTruthy()
+    })
+  })
+
   describe('image', () => {
     test('renders - imageUrl', () => {
       const { container } = render(<SiteMeta imageUrl="/test.jpg" />)
@@ -167,12 +174,164 @@ describe('SiteMeta', () => {
       const { container } = render(<SiteMeta twitterCard="summary" />)
       expect(container.querySelector('[name="twitter:card"]')).toBeTruthy()
     })
+
+    test('renders - twitter:site via twitter.site', () => {
+      const { container } = render(<SiteMeta twitter={{ site: '@test' }} />)
+      expect(container.querySelector('[name="twitter:site"]')).toBeTruthy()
+    })
+
+    test('renders - twitter:creator via twitter.creator', () => {
+      const { container } = render(<SiteMeta twitter={{ creator: '@test' }} />)
+      expect(container.querySelector('[name="twitter:creator"]')).toBeTruthy()
+    })
+
+    test('renders - twitter:card via twitter.card', () => {
+      const { container } = render(<SiteMeta twitter={{ card: 'summary' }} />)
+      expect(container.querySelector('[name="twitter:card"]')).toBeTruthy()
+    })
+
+    test('renders - twitter:app:country', () => {
+      const { container } = render(<SiteMeta twitter={{ app: { country: "US" } }} />)
+      expect(container.querySelector('[name="twitter:app:country"]')).toBeTruthy()
+    })
+
+    test('renders - twitter:app:name:googleplay', () => {
+      const { container } = render(
+        <SiteMeta twitter={{ app: { googlePlay: { name: "Test App Name GooglePlay" } } }} />,
+      )
+      expect(container.querySelector('[name="twitter:app:name:googleplay"]')).toBeTruthy()
+    })
+
+    test('renders - twitter:app:name:googleplay w/ app.name fallback', () => {
+      const { container } = render(
+        <SiteMeta twitter={{ app: { name: "Test App Name", googlePlay: { id: "com.test" } } }} />,
+      )
+      expect(container.querySelector('[name="twitter:app:name:googleplay"]')).toBeTruthy()
+    })
+
+    test('renders - twitter:app:id:googleplay', () => {
+      const { container } = render(
+        <SiteMeta twitter={{ app: { googlePlay: { id: "com.test" } } }} />,
+      )
+      expect(container.querySelector('[name="twitter:app:id:googleplay"]')).toBeTruthy()
+    })
+
+    test('renders - twitter:app:url:googleplay', () => {
+      const { container } = render(
+        <SiteMeta twitter={{ app: { googlePlay: { url: "https://test.com" } } }} />,
+      )
+      expect(container.querySelector('[name="twitter:app:url:googleplay"]')).toBeTruthy()
+    })
+
+    test('renders - twitter:app:name:ipad', () => {
+      const { container } = render(
+        <SiteMeta twitter={{ app: { iPad: { name: "Test App Name iPad" } } }} />,
+      )
+      expect(
+        container.querySelector('[name="twitter:app:name:ipad"]'),
+      ).toBeTruthy()
+    })
+
+    test('renders - twitter:app:name:ipad w/ app.name fallback', () => {
+      const { container } = render(
+        <SiteMeta twitter={{ app: { name: "Test App Name", iPad: { id: "com.test" } } }} />,
+      )
+      expect(
+        container.querySelector('[name="twitter:app:name:ipad"]'),
+      ).toBeTruthy()
+    })
+
+    test('renders - twitter:app:id:ipad', () => {
+      const { container } = render(
+        <SiteMeta twitter={{ app: { iPad: { id: "123456" } } }} />,
+      )
+      expect(container.querySelector('[name="twitter:app:id:ipad"]')).toBeTruthy()
+    })
+
+    test('renders - twitter:app:url:ipad', () => {
+      const { container } = render(
+        <SiteMeta twitter={{ app: { iPad: { url: "https://test.com" } } }} />,
+      )
+      expect(container.querySelector('[name="twitter:app:url:ipad"]')).toBeTruthy()
+    })
+
+    test('renders - twitter:app:name:iphone', () => {
+      const { container } = render(
+        <SiteMeta twitter={{ app: { iPhone: { name: "Test App Name iPhone" } } }} />,
+      )
+      expect(
+        container.querySelector('[name="twitter:app:name:iphone"]'),
+      ).toBeTruthy()
+    })
+
+    test('renders - twitter:app:name:iphone w/ app.name fallback', () => {
+      const { container } = render(
+        <SiteMeta twitter={{ app: { name: "Test App Name", iPhone: { id: "com.test" } } }} />,
+      )
+      expect(
+        container.querySelector('[name="twitter:app:name:iphone"]'),
+      ).toBeTruthy()
+    })
+
+    test('renders - twitter:app:id:iphone', () => {
+      const { container } = render(
+        <SiteMeta twitter={{ app: { iPhone: { id: "123456" } } }} />,
+      )
+      expect(container.querySelector('[name="twitter:app:id:iphone"]')).toBeTruthy()
+    })
+
+    test('renders - twitter:app:url:iphone', () => {
+      const { container } = render(
+        <SiteMeta twitter={{ app: { iPhone: { url: "https://test.com" } } }} />,
+      )
+      expect(container.querySelector('[name="twitter:app:url:iphone"]')).toBeTruthy()
+    })
+
+    test('renders - twitter:player', () => {
+      const { container } = render(<SiteMeta twitter={{ player: { url: "https://test.com" } }} />)
+      expect(container.querySelector('[name="twitter:player"]')).toBeTruthy()
+    })
+
+    test('renders - twitter:player:width', () => {
+      const { container } = render(
+        <SiteMeta twitter={{ player: { url: "https://test.com", width: "100" } }} />,
+      )
+      expect(container.querySelector('[name="twitter:player:width"]')).toBeTruthy()
+    })
+
+    test('renders - twitter:player:height', () => {
+      const { container } = render(
+        <SiteMeta twitter={{ player: { url: "https://test.com", height: "100" } }} />,
+      )
+      expect(container.querySelector('[name="twitter:player:height"]')).toBeTruthy()
+    })
+
+    test('renders - twitter:player:stream', () => {
+      const { container } = render(
+        <SiteMeta twitter={{ player: { url: "https://test.com", stream: { url: "https://test.com/stream" } } }} />,
+      )
+      expect(container.querySelector('[name="twitter:player:stream"]')).toBeTruthy()
+    })
+
+    test('renders - twitter:player:stream:content_type', () => {
+      const { container } = render(
+        <SiteMeta twitter={{ player: { url: "https://test.com", stream: { url: "https://test.com/stream", contentType: "video/mp4" } } }} />,
+      )
+      expect(container.querySelector('[name="twitter:player:stream:content_type"]')).toBeTruthy()
+    })
   })
 
   describe('audio', () => {
     test('renders - audioUrl', () => {
       const { container } = render(<SiteMeta audioUrl="test.mp3" />)
       expect(container.querySelector('[property="og:audio"]')).toBeTruthy()
+    })
+
+    test('renders - audioSecureUrl', () => {
+      const { container } = render(
+        <SiteMeta baseUrl="https://test.com" audioUrl="test.mp3" />,
+      )
+      expect(container.querySelector('[property="og:audio:secure_url"]')).toBeTruthy()
     })
 
     test('renders - audioType', () => {
@@ -187,6 +346,13 @@ describe('SiteMeta', () => {
     test('renders - videoUrl', () => {
       const { container } = render(<SiteMeta videoUrl="/test.mp4" />)
       expect(container.querySelector('[property="og:video"]')).toBeTruthy()
+    })
+
+    test('renders - videoSecureUrl', () => {
+      const { container } = render(
+        <SiteMeta baseUrl="https://test.com" videoUrl="/test.mp4" />,
+      )
+      expect(container.querySelector('[property="og:video:secure_url"]')).toBeTruthy()
     })
 
     test('renders - videoType', () => {
